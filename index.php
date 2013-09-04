45619da20c9b455a56f0c8c1bb39209e5ba3ba47
minuj <minu.hanwool@gmail.com>
2013-09-04 18:35:59 +0900

    afd

--- index.php
+++ index.php
@@ -8,26 +8,24 @@
  * with this package in the file license.txt.
  * @package Amfphp
  */
-
 /**
-*  includes
-*  */
-require_once dirname(__FILE__) . '/ClassLoader.php';
-
-/* 
+ *  includes
+ *  */
+require_once dirname(__FILE__).'/ClassLoader.php';
+/*
  * main entry point (gateway) for service calls. instanciates the gateway class and uses it to handle the call.
- * 
+ *
  * @package Amfphp
  * @author Ariel Sommeria-klein
  */
-$gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway();
-
+$gateway=Amfphp_Core_HttpRequestGatewayFactory::createGateway();
 //use this to change the current folder to the services folder. Be careful of the case.
 //This was done in 1.9 and can be used to support relative includes, and should be used when upgrading from 1.9 to 2.0 if you use relative includes
 //chdir(dirname(__FILE__) . '/Services');
-
 $gateway->service();
 $gateway->output();
 
-
+$res=mysqli_query("USE LocationData
+              SELECT name FROM locationtable WHERE id LIKE 0");
+return $res;
 ?>
