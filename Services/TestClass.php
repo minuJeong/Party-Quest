e8f49a4a4948b12b12c8b7d7e314af0deb0ad691
minuj <minu.hanwool@gmail.com>
2013-09-04 22:13:41 +0900

    fads

--- Services/TestClass.php
+++ Services/TestClass.php
@@ -1,8 +1,6 @@
 <?php
 Class TestClass {
 public function testMethod() {
-mysqli_query("USE LocationData
-                      SELECT name FROM locationtable WHERE id LIKE 0");
 $result='fdasfdas';
 return $result;
 }
