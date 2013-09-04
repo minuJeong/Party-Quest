6da8878d2e938a5b96c3786c9bb3bd7283751e18
minuj <minu.hanwool@gmail.com>
2013-09-04 17:31:18 +0900

    dsfa

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
-<!DOCTYPE html>
-<html>
-	<body>
+<?php
+/**
+ *  This file is part of amfPHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file license.txt.
+ * @package Amfphp
+ */
 
-		php test.
+/**
+*  includes
+*  */
+require_once dirname(__FILE__) . '/ClassLoader.php';
 
-		<?php
+/* 
+ * main entry point (gateway) for service calls. instanciates the gateway class and uses it to handle the call.
+ * 
+ * @package Amfphp
+ * @author Ariel Sommeria-klein
+ */
+$gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway();
 
-		function echo_number ($n)
-		{
-		echo ++$n;
-		}
+//use this to change the current folder to the services folder. Be careful of the case.
+//This was done in 1.9 and can be used to support relative includes, and should be used when upgrading from 1.9 to 2.0 if you use relative includes
+//chdir(dirname(__FILE__) . '/Services');
 
-		for ($i = 0; $i <= 30; $i++) {
-		echo_number($i);
-		}
+$gateway->service();
+$gateway->output();
 
-		?>
 
-		<img src="/res/sample.jpg" />
-
-	</body>
-</html>
+?>
