cd90dec7958908461086bac373a8fc7e2ece5700
minuj <minu.hanwool@gmail.com>
2013-09-04 18:29:00 +0900

    fads

--- Services/TestClass.php
+++ Services/TestClass.php
@@ -2,7 +2,7 @@
 Class TestClass {
 public function testMethod() {
 mysqli_query("USE LocationData");
-$result=mysqli_query("SELECT 'name' FROM locationtable");
+$result=mysqli_query("SELECT `name` FROM locationtable");
 return $result;
 }
 }
