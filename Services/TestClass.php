846cf0aba6b6bd0cd44620b0469a636c502666e0
minuj <minu.hanwool@gmail.com>
2013-09-04 19:55:34 +0900

    reset TestClass.php

--- Services/TestClass.php
+++ Services/TestClass.php
@@ -1,8 +1,9 @@
 <?php
 Class TestClass {
 public function testMethod() {
-$result=mysqli_query("USE LocationData
+mysqli_query("USE LocationData
                       SELECT name FROM locationtable WHERE id LIKE 0");
+$result='fdasfdas';
 return $result;
 }
 }
