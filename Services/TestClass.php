8a30c23bc270bd9266ddb40a638e2e5f1dbf6215
minuj <minu.hanwool@gmail.com>
2013-09-05 15:39:29 +0900

    updated TestClass.php

--- Services/TestClass.php
+++ Services/TestClass.php
@@ -1,8 +1,20 @@
 <?php
 Class TestClass {
-public function testMethod() {
-$result='fdasfd1111111as';
-return $result;
-}
+	public function testMethod() {
+
+		$HOST = "localhost:3306";
+		$DBNAME = "LocationData";
+		$DBUSER = "root";
+		$DBPASS = "A8057silent";
+
+		$conn = mysqli_connect("$HOST", "$DBUSER", "$DBPASS");
+		mysqli_select_db("$DBNAME", $conn);
+
+		$QUERY = "SELECT * FROM locationtable";
+
+		$result = mysqli_query($QUERY);
+		return $result;
+	}
+
 }
 ?>
