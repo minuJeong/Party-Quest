5a03a9b3cd5a6b6594bef7cb29467ce2be89c140
minuj <minu.hanwool@gmail.com>
2013-09-05 16:45:53 +0900

    1645

--- Services/SQLLink.php
+++ Services/SQLLink.php
@@ -1,31 +1,24 @@
 <?php
 Class SQLLink {
 
-	private static $HOST = "localhost";
+	private static $HOST = "127.0.0.1";
 	private static $DBUSER = "root";
 	private static $DBPASS = "A8057silent";
 
 	private $DBNAME = "LocationData";
 
 	public function SQLLink() {
-		$conn = mysqli_connect($HOST, $DBUSER, $DBPASS);
-		mysqli_select_db($DBNAME);
-		echo mysqli_query("DESCRIBE locationtable;");
-		$query = "SELECT * FROM locationtable;";
-		$result = mysqli_query($query);
-		return $result;
-		
-		echo $result;
 	}
 
 	public function getResult($param) {
 
 		$conn = mysqli_connect($HOST, $DBUSER, $DBPASS);
 		mysqli_select_db($DBNAME);
-		echo mysqli_query("DESCRIBE locationtable;");
+
 		$query = "SELECT * FROM locationtable;";
 		$result = mysqli_query($query);
-		return $result;
+		
+		return $result . $param;
 	}
 
 }
