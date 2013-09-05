eae3df8e5f02641d03fa41253fde13ac0e0175ca
minuj <minu.hanwool@gmail.com>
2013-09-05 16:37:11 +0900

    SQLLINK.php updated 1637

--- Services/SQLLink.php
+++ Services/SQLLink.php
@@ -13,7 +13,9 @@ Class SQLLink {
 
 	public function getResult($param) {
 
-		$conn = mysqli_connect($HOST, $DBUSER, $DBPASS, $DBNAME);
+		$conn = mysqli_connect($HOST, $DBUSER, $DBPASS);
+		mysqli_select_db($DBNAME);
+		echo mysqli_query("DESCRIBE locationtable;");
 		$query = "SELECT * FROM locationtable;";
 		$result = mysqli_query($query);
 		return $result;
