15d0deb456a68876abe0058549da3b50b523b4c4
minuj <minu.hanwool@gmail.com>
2013-09-05 17:48:44 +0900

    tester.php updated. 1748

--- tester.php
+++ tester.php
@@ -1,5 +1,9 @@
 <?php
 
+echo '
+start point
+';
+
 $HOST = "127.0.0.1";
 $USER = "root";
 $PW = "A8057silent";
@@ -8,10 +12,22 @@ $DB = "LocationData";
 $conn = mysqli_connect($HOST, $USER, $PW, $DB);
 if (mysqli_errno($conn)) {
 	echo mysqli_error();
+} else {
+	echo '
+	connection successful.
+	';
 }
+
 $que = "SELECT name FROM locationtable;";
 $queryResult = mysqli_query($conn, $que);
-echo mysqli_data_seek($queryResult, 0);
 
-echo 'end point';
+echo $queryResult . '
+';
+
+echo mysqli_data_seek($queryResult, 0) . '
+';
+
+echo '
+end point
+';
 ?>
