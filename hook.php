d5b69f5733b5d10c65ca29a753b7538a90fae707
minuj <minu.hanwool@gmail.com>
2013-09-06 14:57:21 +0900

    Update hook.php

--- hook.php
+++ hook.php
@@ -5,10 +5,18 @@ $LOCAL_ROOT         = "/var/pqlhamfphp";
 
 // Delete local repo if it exists
 if (file_exists($LOCAL_ROOT)) {
+    echo 'root exists. removing..';
     shell_exec("sudo rm -r {$LOCAL_ROOT}");
 }
 
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
+echo 'cloning..';
 shell_exec("cd {$LOCAL_ROOT} && cd .. && sudo git clone https://github.com/minuJeong/pqlhamfphp.git");
+
+echo '
+';
+
+echo 'hook.php complete.
+';
 
 ?>
