1635ddee618f6a4e13bac12f551be849e0f3aa2d
minuj <minu.hanwool@gmail.com>
2013-09-06 15:32:14 +0900

    Update mysqlData.php

--- mysqlData.php
+++ mysqlData.php
@@ -5,6 +5,15 @@ $host = '127.0.0.1';
 $usr = 'root';
 $pw = 'A8057silent';
 
-$conn = mysqli_connect($host, $usr, $pw);
+$conn = mysqli_connect($host, $usr, $pw, 'LocationData');
+echo 'describe locations..
+';
+$query = 'DESCRIBE locations';
+echo mysqli_query($conn, $query);
+
+echo 'SELECT * FROM locations..
+';
+$query = 'SELECT * FROM locations';
+echo mysqli_query($conn, $query);
 
 ?>
