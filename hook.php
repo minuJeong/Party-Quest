cf719ff33ba6c987f3a9985948d9c200b4af9a48
minuj <minu.hanwool@gmail.com>
2013-09-06 15:03:10 +0900

    Update hook.php

--- hook.php
+++ hook.php
@@ -7,7 +7,7 @@ $CLONE_ROOT         = "/var/";
 // Delete local repo if it exists
 if (file_exists($LOCAL_ROOT)) {
     echo 'root exists. removing..';
-    shell_exec("sudo rm -r {$LOCAL_ROOT}");
+    shell_exec("sudo rm -rf {$LOCAL_ROOT}");
 }
 
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
