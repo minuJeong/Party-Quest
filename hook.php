506c814582f2d0b475d316f73056879c53246443
minuj <minu.hanwool@gmail.com>
2013-09-09 11:17:20 +0900

    Update hook.php

--- hook.php
+++ hook.php
@@ -6,16 +6,22 @@ $CLONE_ROOT         = "/var/";
 // Delete local repo if it exists
 if (file_exists($CLONE_ROOT . '/pq')) {
     echo 'root exists.
-    removing..
-    ';
+removing..
+';
     shell_exec("sudo rm -rf {$CLONE_ROOT}/pq");
 }
 
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
-echo 'cloning..
+echo 'cd ~~..
 ';
 shell_exec("cd {$CLONE_ROOT}");
+
+echo 'git clone ~~
+';
 shell_exec("sudo git clone https://github.com/minuJeong/pq.git");
+
+echo 'chmod.
+';
 shell_exec("sudo chmod 777 {$CLONE_ROOT}/pq");
 
 echo '
