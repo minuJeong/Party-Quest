59ad6a4d0fe66f869f6462a908e3b6132d4f67a6
minuj <minu.hanwool@gmail.com>
2013-09-09 12:17:49 +0900

    123

--- Services/SQLLink.php
+++ Services/SQLLink.php
@@ -20,38 +20,12 @@ Class SQLLink {
 		$result = "";
 
 		$conn = mysqli_connect($HOST, $DBUSER, $DBPASS);
-
-		if (mysqli_connect_errno($conn)) {
-			return "Failed connection to mysql. error: " . mysqli_error();
-		}
-
-		$result . "conncetion successful.  ";
-
 		mysqli_select_db($DBNAME);
 
-		$query . 'SELECT ';
-		if (isset($param)) {
-			$query . ' ' . $param;
-		} else {
-			$query . "*";
-		}
+		$query = "SELECT * FROM locations";
+		$result . mysqli_query($conn, $query);
 
-		$query . ' FROM `locations`;';
-
-		if ($dataset = mysqli_query($conn, $query)) {
-
-			$datalen = mysqli_affected_rows();
-			$result . "number of " . $datalen . "found.  ";
-
-			$name = "";
-			for ($i = 0; $i < $datalen; $i++) {
-				mysqli_data_seek($dataset, $i);
-				$row = mysqli_fetch_row($dataset);
-				$name . $location[name];
-			}
-		}
-
-		return $result . $name . $param;
+		return $result;
 	}
 
 }
