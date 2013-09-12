5b6d98f1c4c495a61ddff93fb28e6fc2a9b924da
minuj <minu.hanwool@gmail.com>
2013-09-12 17:50:43 +0900

    commitment -> after flash builder

--- Services/SQLQuery.php
+++ Services/SQLQuery.php
@@ -9,14 +9,14 @@ class SQLQuery {
 		$sqluser = SQLLoginData::getId();
 		$sqlpass = SQLLoginData::getPass();
 
-		$this -> $conn = mysqli_connect($sqlhost, $sqluser, $sqlpass);
+		$this -> conn = mysqli_connect($sqlhost, $sqluser, $sqlpass);
 		mysqli_select_db('LocationData');
 
 	}
 
 	public function query($query) {
 
-		$ret = mysqli_query($query);
+		$ret = mysqli_query($this -> conn, $query);
 
 		return $ret;
 
